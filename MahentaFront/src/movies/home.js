9815b8c49a26f11441bfe59f2880d5f63c37b18f
tafka96 <t.martoja@gmail.com>
2018-02-27 14:06:09 +0200

    bootstrap

--- MahentaFront/src/movies/home.js
+++ MahentaFront/src/movies/home.js
@@ -1,18 +1,18 @@
 import 'bootstrap';
-// import {HttpClient} from 'aurelia-fetch-client'
+import {HttpClient} from 'aurelia-fetch-client'
 
 export class Home {
 
- //    movieList = []
+     movieList = []
     
- //    activate(){
- //        let client = new HttpClient();
- //        client.fetch('http://localhost:8080/movies')
-	// 		.then(response => response.json())
-	// 		.then(movies => this.movieList = movies);
- //    }
+     activate(){
+         let client = new HttpClient();
+       client.fetch('http://localhost:8080/movies')
+	 		.then(response => response.json())
+	 		.then(movies => this.movieList = movies);
+     }
     
-	// constructor() {
-	// 	this.message = "tulemast filme vaatama"
-	// }
+    constructor() {
+	this.message = "tulemast filme vaatama"
+	}
 }
