3ed0b8392b3d462a010337e1d8d5a288eb6a079f
henlep <henrilepik@gmail.com>
2018-03-04 16:52:59 +0200

    rest paring client.fetch url puudu hetkel

--- MahentaFront/src/sessions/session.js
+++ MahentaFront/src/sessions/session.js
@@ -1,6 +1,24 @@
-import 'bootstrap';
-import {HttpClient} from 'aurelia-fetch-client'
+import 'bootstrap'
+import {HttpClient, json} from 'aurelia-fetch-client'
 
-export class Session{
+export class session{
+
+  sessionData = {}
+
+  addSession(){
+
+    let client = new HttpClient();
+
+
+    client.fetch('', {
+      'method': "POST",
+      'body': json(this.sessionData)
+    })
+      .then(response => response.json())
+      .then(data =>{
+        console.log("saadeti" + data.MovieTitle);
+      });
+    console.log("method execute")
+  }
 
 }
