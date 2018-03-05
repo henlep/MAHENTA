ad1e8f895d62cbd6c8e7db2f19ed7b755f629e1d
tafka96 <t.martoja@gmail.com>
2018-03-05 22:09:42 +0200

    sessions fix

--- Mahenta/src/main/java/com/MahentaRestApp/Mahenta/Session/SessionController.java
+++ Mahenta/src/main/java/com/MahentaRestApp/Mahenta/Session/SessionController.java
@@ -5,10 +5,7 @@ import com.MahentaRestApp.Mahenta.Movie.Movie;
 import com.MahentaRestApp.Mahenta.Movie.MovieController;
 import com.MahentaRestApp.Mahenta.Movie.MovieRepository;
 import com.MahentaRestApp.Mahenta.Movie.MovieService;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -32,15 +29,12 @@ public class SessionController {
 
     @RequestMapping(value="/sessions/add", method=RequestMethod.POST,
             consumes = "application/json")
-    public Session addSession(@RequestBody Session session, long movie_id) {
+    public Session addSession(@RequestBody Session session) {
 
 
         return sessionService.addSession(session);
     }
 
-    @RequestMapping(value = "//{id}", method=RequestMethod.GET)
-    public Movie getMovie(@PathVariable("id") long movieId) {
-        return movieService.getMovieById(movieId);
-    }
+
 
 }
--- MahentaFront/src/sessions/session.js
+++ MahentaFront/src/sessions/session.js
@@ -3,19 +3,28 @@ import {HttpClient, json} from 'aurelia-fetch-client'
 export class session{
 
   sessionData = {}
+    movieList=[]
+    
+    constructor() {
+        let client = new HttpClient();
+        client.fetch('http://localhost:8080/movies')
+			.then(response => response.json())
+			.then(movies => this.movieList = movies);
+        
+    }
 
   addSession(){
 
     let client = new HttpClient();
 
 
-    client.fetch('', {
+    client.fetch('http://localhost:8080/sessions/add', {
       'method': "POST",
       'body': json(this.sessionData)
     })
       .then(response => response.json())
       .then(data =>{
-        console.log("Server saatis" + data.MovieTitle);
+        console.log("Server saatis" + data.movie_id);
       });
     console.log("method executed")
   }
