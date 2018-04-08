1c17a91e57e66d70bc7c6192e41d74d4e7d17a83
henlep <henrilepik@gmail.com>
2018-04-08 18:40:37 +0300

    pushi jama proov

--- Mahenta/src/main/java/com/MahentaRestApp/Mahenta/Show/ShowController.java
+++ Mahenta/src/main/java/com/MahentaRestApp/Mahenta/Show/ShowController.java
@@ -23,14 +23,14 @@ public class ShowController {
 
 
     //ARa seda järgnevat lisa
-    @RequestMapping(value="/updateShows")
-    public Show UpdateShows() throws IOException, SAXException, ParserConfigurationException {
-        Show show = new Show();
-        SessionsXMLParser sessionsXMLParser = new SessionsXMLParser();
-        List<Show> shows = sessionsXMLParser.GetItems();
-        for (int i = 0; i<shows.size(); i++){
-            show = showService.addShow(shows.get(i));
-        }
-        return shows.get(0);
-    }
+ //   @RequestMapping(value="/updateShows")
+   // public Show UpdateShows() throws IOException, SAXException, ParserConfigurationException {
+    //    Show show = new Show();
+     //   SessionsXMLParser sessionsXMLParser = new SessionsXMLParser();
+     //   List<Show> shows = sessionsXMLParser.GetItems();
+      //  for (int i = 0; i<shows.size(); i++){
+       //     show = showService.addShow(shows.get(i));
+      //  }
+      //  return shows.get(0);
+  //  }
 }
