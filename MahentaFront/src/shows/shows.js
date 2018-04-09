e513e6e7ec36ba4b1c41014a3279dc8087bff9d6
tafka96 <t.martoja@gmail.com>
2018-04-09 16:27:34 +0300

    datepicker

--- MahentaFront/src/shows/shows.js
+++ MahentaFront/src/shows/shows.js
@@ -4,28 +4,57 @@ import {inject, bindable} from 'aurelia-framework';
 export class Home {
     showList = [];
     cinemaList = [];
+    minDate;
+    maxDate;
 
     @bindable selectedCinema = "";
     @bindable searchTerm = "";
+    @bindable selectedDate = "";
     
     activate(){
+        
+        
+        var today = new Date().toISOString();
+        var dateS = today.substring(0,10);
+        this.minDate=dateS
+        
+        var maxDay = new Date();
+        
+        maxDay = new Date(maxDay.getTime()+(2*24*60*60*1000)).toISOString();
+        var maxS = maxDay.substring(0,10);
+        console.log(maxS)
+        this.maxDate = maxS;
+        
+        
         let client = new HttpClient();
-        client.fetch('http://localhost:8080/showsDate?date=2018-04-09')
+        client.fetch('http://localhost:8080/showsDate?date='+dateS)
 			.then(response => response.json())
 			.then(shows => this.showList = shows);
         
-        client.fetch('http://localhost:8080/cinemas?date=2018-04-09')
+        client.fetch('http://localhost:8080/cinemas?date='+dateS)
 			.then(response => response.json())
-			.then(cinemas => this.cinemaList = cinemas);
-    
-
-        
-        
-        
+			.then(cinemas => this.cinemaList = cinemas);     
         
         
      }
 
+    getMin(){
+        return "2018-04-08"
+    }
+
+    dateChanged(date){
+        this.showList = [];
+        this.cinemaList = [];
+        let client = new HttpClient();
+        client.fetch('http://localhost:8080/showsDate?date='+date)
+			.then(response => response.json())
+			.then(shows => this.showList = shows);
+        
+        client.fetch('http://localhost:8080/cinemas?date='+date)
+			.then(response => response.json())
+			.then(cinemas => this.cinemaList = cinemas); 
+    }
+    
     openPage(url){
         var win = window.open(url, '_blank');
         win.focus();
@@ -57,14 +86,7 @@ export class Home {
      
   }
 
-    cinemaFunc(cinema, value){
-                
-        console.log("ss")
-        matches = value.toUpperCase().indexOf(cinema.toUpperCase()) !== -1 ;
-      
-    
-     return matches
-    }
+   
     
     
 }
