9e20b5d5d1824814d1cd0591021a8ccf988d4143
Unknown <makesk@ttu.ee>
2018-05-14 22:21:16 +0300

    show repo test

--- Mahenta/src/test/java/com/MahentaRestApp/Mahenta/RepositoryTest.java
+++ Mahenta/src/test/java/com/MahentaRestApp/Mahenta/RepositoryTest.java
@@ -2,6 +2,8 @@ package com.MahentaRestApp.Mahenta;
 
 import com.MahentaRestApp.Mahenta.Movie.Movie;
 import com.MahentaRestApp.Mahenta.Movie.MovieRepository;
+import com.MahentaRestApp.Mahenta.Show.Show;
+import com.MahentaRestApp.Mahenta.Show.ShowRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,6 +23,19 @@ public class RepositoryTest {
 
         public void myTest() throws Exception {
             movieRepository.save(new Movie());
+        }
+    }
+    @RunWith(SpringRunner.class)
+    @SpringBootTest
+    public class ManagementTest {
+
+        @Autowired
+        private ShowRepository showRepository;
+
+        @Test
+
+        public void myShowRepoTest() throws Exception {
+            showRepository.save(new Show());
         }
     }
 }
